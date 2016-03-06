49fd383277a5977e1678e5ecb132e2cf89fa8acc
CSchultz <Carter12s@msn.com>
2016-03-06 13:19:40 -0700

    Logging Working

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
+#define ELPP_NO_DEFAULT_LOG_FILE
+#define ELPP_STL_LOGGING
+
 #include<iostream>
+#include <easylogging++.h>
+
+INITIALIZE_EASYLOGGINGPP
 
 int main(){
+    el::Configurations conf("../logging.conf");
+    el::Loggers::reconfigureAllLoggers(conf);
+    el::Loggers::addFlag(el::LoggingFlag::ColoredTerminalOutput);
+    LOG(ERROR) << "Hello World" << std::endl;
 	std::cout << "Hello World" << std::endl;
 }
